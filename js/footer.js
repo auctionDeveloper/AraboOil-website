ac1ba375c608872f95eb6dba0d68baf1d2190050
auctionDeveloper <auctionwale.bank@gmail.com>
2025-05-26 18:41:18 +0530

    commmit

--- js/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('./components/footer.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('footer-placeholder').innerHTML = data;
-    })
-    .catch(err => console.error('Footer loading error:', err));
-});
--- js/script.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.hero-content button').addEventListener('click', () => {
-  alert('Contact us clicked! You can add your contact form or redirect here.');
-});
