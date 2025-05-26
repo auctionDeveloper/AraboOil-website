6f03f7f5729065bf2f137b61baee127930f31740
auctionDeveloper <auctionwale.bank@gmail.com>
2025-05-26 18:43:05 +0530

    Updates

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('./components/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar-placeholder').innerHTML = data;
-    })
-    .catch(err => console.error('Navbar loading error:', err));
-});
